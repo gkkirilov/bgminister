b66765ef2380f705703f59a4c35d5694d10d9b9c
Georgi Kirilov <gkkirilov@gmail.com>
2024-03-20 13:19:58 +0200

    Update

--- app.vue
+++ app.vue
@@ -31,6 +31,11 @@
           <button @click="submit"
             class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Провери</button>
         </div>
+
+        <a href="https://twitter.com/Georgi_________" class="text-gray-400 flex justify-center items-center gap-x-2 mt-3 fixed left-0 right-0 bottom-10">
+          Created by Georgi
+          <img src="/x.svg" class="h-4">
+        </a>
       </div>
       <Transition name="slide-fade">
 
